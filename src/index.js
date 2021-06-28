f1373743e6e1a10c001c08831064e5bdc7be5562
Ayassaka <jack.zwx@icloud.com>
2021-06-28 15:31:31 -0400

    shapePool implemented

--- src/index.js
+++ src/index.js
@@ -1,135 +1,176 @@
 import * as THREE from 'three';
 import { saveAs } from 'file-saver';
+import font from 'url:./arial-unicode-ms.ttf'
 
-import fira from './FiraSansHeavyRegular.json'
 import STLExporter from './STLExporter.js'
 
-const radius = 6;
+import TextToSVG from 'text-to-svg';
+let textToSVG;
+TextToSVG.load(font, function(err, t) { textToSVG = t; });
 
-let scene = new THREE.Scene();
+import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 
-let renderer = new THREE.WebGLRenderer( { antialias: true } );
-let rendererWidth = window.innerWidth;
-let rendererHeight = window.innerHeight;
-renderer.setSize(rendererWidth, rendererHeight);
+const previewLoopRadius = 6;
 
-let camera = new THREE.PerspectiveCamera(70, rendererWidth / rendererHeight, 0.01, 20);
-camera.position.x = radius;
-camera.position.y = 0;
-camera.position.z = 12;
-camera.lookAt(0, 0, 0);
-renderer.setAnimationLoop(time => {
-	camera.position.x = radius * Math.cos( time / 2000 );
-	camera.position.y = radius * Math.sin( time / 2000 );
-	camera.lookAt(0, 0, 0);
-	renderer.render( scene, camera );
-});
-document.body.appendChild( renderer.domElement );
-
-let currentMesh = loadText("Meow", new THREE.Font(fira));
+let scene, renderer, rendererWidth, footer, rendererHeight, camera, currentObject;
 
 const maxSize = 10;
 
-addGround();
-addMoveHandlers();
+loadUIBar();
+loadRenderer();
+loadGround();
+loadMoveHandlers();
 
-function addGround() {
-	const geometry = new THREE.PlaneGeometry(maxSize, maxSize, 10, 10);
-	// const material = new THREE.MeshNormalMaterial();
-	// const plane = new THREE.Mesh( geometry, material );
-	// scene.add( plane );
-
-	const wireframe = new THREE.WireframeGeometry(geometry);
-	const line = new THREE.LineSegments( wireframe );
-	line.material.depthTest = true;
-	scene.add( line );
+function loadRenderer() {
+  scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  rendererWidth = window.innerWidth;
+  footer = document.getElementById("footer");
+  rendererHeight = window.innerHeight - footer.offsetHeight;
+  renderer.setSize(rendererWidth, rendererHeight);
+  
+  camera = new THREE.PerspectiveCamera(70, rendererWidth / rendererHeight, 0.01, 20);
+  camera.position.x = previewLoopRadius;
+  camera.position.y = 0;
+  camera.position.z = 12;
+  camera.lookAt(0, 0, 0);
+  renderer.setAnimationLoop(time => {
+    camera.position.x = previewLoopRadius * Math.cos( time / 2000 );
+    camera.position.y = previewLoopRadius * Math.sin( time / 2000 );
+    camera.lookAt(0, 0, 0);
+    renderer.render( scene, camera );
+  });
+  footer.prepend(renderer.domElement);
 }
 
-function loadText(string, font) {
-	const geometry = new THREE.TextGeometry(string, {
-		font: font,
-		size: 2,
-		height: 1,
-		curveSegments: 12,
-	});
-	const material = new THREE.MeshNormalMaterial();
-	const mesh = new THREE.Mesh( geometry, material );
-	mesh.position.x = -4;
-	mesh.position.z = 0;
-	scene.add( mesh );
-	return mesh;
+function loadGround() {
+  const geometry = new THREE.PlaneGeometry(maxSize, maxSize, 10, 10);
+  const wireframe = new THREE.WireframeGeometry(geometry);
+  const line = new THREE.LineSegments( wireframe );
+  line.material.depthTest = true;
+  scene.add( line );
 }
 
 function saveStl() {
-	let exporter = new STLExporter();
-	let str = exporter.parse( scene ); // Export the scene
-	var blob = new Blob( [str], { type : 'text/plain' } ); // Generate Blob from the string
-	saveAs( blob, 'file.stl' ); //Save the Blob to file.stl
+  let exporter = new STLExporter();
+  let str = exporter.parse( scene ); // Export the scene
+  var blob = new Blob( [str], { type : 'text/plain' } ); // Generate Blob from the string
+  saveAs( blob, 'file.stl' ); //Save the Blob to file.stl
 }
 
 
-function addMoveHandlers() {
-	
-	renderer.domElement.addEventListener("touchstart", handleMoveZoom);
-	renderer.domElement.addEventListener("touchmove", handleMoveZoom);
+function loadMoveHandlers() {
+  renderer.domElement.addEventListener("touchstart", handleMoveZoom);
+  renderer.domElement.addEventListener("touchmove", handleMoveZoom);
 
-	let tpCache = {};
+  let tpCache = {};
 
-	function handleMoveZoom(e) {
-		// don't handle 3-touch
-		if (e.targetTouches.length > 2) {
-			tpCache = {};
-			return;
-		}
+  function handleMoveZoom(e) {
+    // don't handle 3-touch
+    if (!currentObject || e.targetTouches.length > 2) {
+      tpCache = {};
+      return;
+    }
 
-		let movedTps = [];
-		for (let i = 0; i < e.targetTouches.length; i++) {
-			let tp = e.targetTouches[i];
-			let id = tp.identifier;
-			if (tpCache[id] !== undefined) {
-				movedTps.push(tp);
-			}
-		}
-		
-		// document.getElementById("test").innerHTML = movedTps.map(tp => tp.identifier).join(',');
-		
-		if (movedTps.length === 1) {
-			let p = movedTps[0];
-			currentMesh.position.x += (p.clientX - tpCache[p.identifier].clientX) / rendererHeight * maxSize;
-			currentMesh.position.y -= (p.clientY - tpCache[p.identifier].clientY) / rendererHeight * maxSize;
-		} else if (movedTps.length === 2) {
-			let p1 = movedTps[0];
-			let p2 = movedTps[1];
-			let x1 = tpCache[p1.identifier].clientX / rendererHeight * maxSize - maxSize / 2;
-			let x2 = tpCache[p2.identifier].clientX / rendererHeight * maxSize - maxSize / 2;
-			let y1 = - tpCache[p1.identifier].clientY / rendererHeight * maxSize + maxSize / 2;
-			let y2 = - tpCache[p2.identifier].clientY / rendererHeight * maxSize + maxSize / 2;
-			let x3 = p1.clientX / rendererHeight * maxSize - maxSize / 2;
-			let x4 = p2.clientX / rendererHeight * maxSize - maxSize / 2;
-			let y3 = - p1.clientY / rendererHeight * maxSize + maxSize / 2;
-			let y4 = - p2.clientY / rendererHeight * maxSize + maxSize / 2;
-			let n1x = x2 - x1;
-			let n1y = y2 - y1;
-			let n2x = x4 - x3;
-			let n2y = y4 - y3;
-			let kx = currentMesh.position.x - x1;
-			let ky = currentMesh.position.y - y1;
-			let a = Math.atan2(n2y, n2x) - Math.atan2(n1y, n1x);
-			let r = Math.sqrt(n2x*n2x + n2y*n2y) / Math.sqrt(n1x*n1x + n1y*n1y);
-			currentMesh.position.x = (Math.cos(a) * kx - Math.sin(a) * ky) * r + x3;
-			currentMesh.position.y = (Math.sin(a) * kx + Math.cos(a) * ky) * r + y3;
-			currentMesh.rotation.z += a;
-			currentMesh.scale.x *= r;
-			currentMesh.scale.y *= r;
-			// document.getElementById("test").innerHTML = `${[n1x, n1y, n2x, n2y].join('+')}`;
-		}
+    let movedTps = [];
+    for (let i = 0; i < e.targetTouches.length; i++) {
+      let tp = e.targetTouches[i];
+      let id = tp.identifier;
+      if (tpCache[id] !== undefined) {
+        movedTps.push(tp);
+      }
+    }
 
-		
-		tpCache = {};
-		for (let i = 0; i < e.targetTouches.length; i++) {
-			let tp = e.targetTouches[i];
-			let id = tp.identifier;
-			tpCache[id] = tp;
-		}
-	}
+    if (movedTps.length === 1) {
+      let p = movedTps[0];
+      currentObject.position.x += (p.clientX - tpCache[p.identifier].clientX) / rendererHeight * maxSize;
+      currentObject.position.y -= (p.clientY - tpCache[p.identifier].clientY) / rendererHeight * maxSize;
+    } else if (movedTps.length === 2) {
+      let p1 = movedTps[0];
+      let p2 = movedTps[1];
+      let x1 = tpCache[p1.identifier].clientX / rendererHeight * maxSize - maxSize / 2;
+      let x2 = tpCache[p2.identifier].clientX / rendererHeight * maxSize - maxSize / 2;
+      let y1 = - tpCache[p1.identifier].clientY / rendererHeight * maxSize + maxSize / 2;
+      let y2 = - tpCache[p2.identifier].clientY / rendererHeight * maxSize + maxSize / 2;
+      let x3 = p1.clientX / rendererHeight * maxSize - maxSize / 2;
+      let x4 = p2.clientX / rendererHeight * maxSize - maxSize / 2;
+      let y3 = - p1.clientY / rendererHeight * maxSize + maxSize / 2;
+      let y4 = - p2.clientY / rendererHeight * maxSize + maxSize / 2;
+      let n1x = x2 - x1;
+      let n1y = y2 - y1;
+      let n2x = x4 - x3;
+      let n2y = y4 - y3;
+      let kx = currentObject.position.x - x1;
+      let ky = currentObject.position.y - y1;
+      let a = Math.atan2(n2y, n2x) - Math.atan2(n1y, n1x);
+      let r = Math.sqrt(n2x*n2x + n2y*n2y) / Math.sqrt(n1x*n1x + n1y*n1y);
+      currentObject.position.x = (Math.cos(a) * kx - Math.sin(a) * ky) * r + x3;
+      currentObject.position.y = (Math.sin(a) * kx + Math.cos(a) * ky) * r + y3;
+      currentObject.rotation.z += a;
+      currentObject.scale.x *= r;
+      currentObject.scale.y *= r;
+    }
+
+    tpCache = {};
+    for (let i = 0; i < e.targetTouches.length; i++) {
+      let tp = e.targetTouches[i];
+      let id = tp.identifier;
+      tpCache[id] = tp;
+    }
+  }
+}
+
+function loadUIBar() {
+  const shapes = ['★','☆','●','○','■','□','A'];
+  const shapePool = document.getElementById('shapePool');
+  shapes.map(text => {
+    console.log(text);
+    const div = document.createElement('div');
+    div.className = 'shape';
+    div.textContent = text;
+    div.onclick = e => {
+      currentObject = addText(e.target.textContent);
+    };
+    shapePool.appendChild(div);
+  });
+}
+
+function addText(text) {
+  if (!textToSVG) return;
+  const options = {x: 0, y: 0, fontSize: 4, anchor: 'center middle', attributes: {fill: "black"}};
+  const svg = textToSVG.getSVG(text, options);
+  let obj = extrudeSvg(svg);
+  scene.add(obj);
+  return obj;
+}
+
+function extrudeSvg(svg) {
+  const loader = new SVGLoader();
+  const svgData = loader.parse(svg);
+  console.log(svgData);
+
+  // Group that will contain all of our paths
+  const svgGroup = new THREE.Group();
+
+  const material = new THREE.MeshNormalMaterial();
+
+  // Loop through all of the parsed paths
+  svgData.paths.forEach((path, i) => {
+    const shapes = path.toShapes(true);
+
+    // Each path has array of shapes
+    shapes.forEach((shape, j) => {
+      // Finally we can take each shape and extrude it
+      const geometry = new THREE.ExtrudeGeometry(shape, {
+        depth: 1,
+        bevelEnabled: false
+      });
+
+      // Create a mesh and add it to the group
+      const mesh = new THREE.Mesh(geometry, material);
+      svgGroup.add(mesh);
+    });
+  });
+  svgGroup.scale.y *= -1;
+  return svgGroup;
 }
