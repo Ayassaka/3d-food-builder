213111801dc360d462f73e1854a09631fd19771a
Ayassaka <jack.zwx@icloud.com>
2021-06-29 03:33:44 -0400

    basic features

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { saveAs } from 'file-saver';
-import font from 'url:./arial-unicode-ms.ttf'
+import font from 'url:./arial-unicode-ms.ttf' // TODO: allow cache
 
 import STLExporter from './STLExporter'
 import WebkitInputRangeFillLower from './webkit-input-range-fill-lower' 
@@ -11,17 +11,21 @@ TextToSVG.load(font, function(err, t) { textToSVG = t; });
 
 import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 
-const previewLoopRadius = 2;
+const previewLoopRadius = 4;
 
-let scene, renderer, rendererWidth, footer, rendererHeight, camera, currentObject, material;
+let scene, renderer, rendererWidth, footer, rendererHeight, camera, material;
+
+let currentObject, objectList = [];
+
+let aspectSlider, heightSlider;
 
 const maxSize = 10;
 
-loadUIBar();
+loadUI();
+loadMaterial();
 loadRenderer();
 loadGround();
 loadMoveHandlers();
-loadMaterial();
 
 function loadRenderer() {
   scene = new THREE.Scene();
@@ -31,23 +35,40 @@ function loadRenderer() {
   footer = document.getElementById("footer");
   rendererHeight = window.innerHeight - footer.offsetHeight - header.offsetHeight;
   renderer.setSize(rendererWidth, rendererHeight);
+  renderer.shadowMap.enabled = true;
   
-  camera = new THREE.PerspectiveCamera(70, rendererWidth / rendererHeight, 0.01, 20);
+  camera = new THREE.PerspectiveCamera(50, rendererWidth / rendererHeight, 0.01, 25);
   camera.position.x = previewLoopRadius;
   camera.position.y = 0;
-  camera.position.z = previewLoopRadius * 5;
+  camera.position.z = previewLoopRadius * 4;
   camera.lookAt(0, 0, 0);
   renderer.setAnimationLoop(time => {
     camera.position.x = previewLoopRadius * Math.cos( time / 2000 );
     camera.position.y = previewLoopRadius * Math.sin( time / 2000 );
     camera.lookAt(0, 0, 0);
+    camera.updateProjectionMatrix();
     renderer.render( scene, camera );
   });
   document.getElementById("main").prepend(renderer.domElement);
+
+  let hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 1);
+  // hemiLight.castShadow = true;
+  scene.add(hemiLight);
+  let light = new THREE.PointLight(0xffffff, 1);
+  light.position.set(-maxSize * 1.5, maxSize * 1.5, maxSize * 1.5);
+  light.castShadow = true;
+  light.shadow.mapSize.width = 1024;
+  light.shadow.mapSize.height = 1024;
+  scene.add(light);
 }
 
 function loadMaterial() {
-  material = new THREE.MeshNormalMaterial();
+  material = new THREE.MeshPhongMaterial({
+    color: '#7B3F00',
+    shininess: 10,
+    specular: 0x5f5f5f
+  });
+  material.flatShading = false;
 }
 
 function loadGround() {
@@ -57,14 +78,6 @@ function loadGround() {
   line.material.depthTest = true;
   scene.add( line );
 }
-
-function saveStl() {
-  let exporter = new STLExporter();
-  let str = exporter.parse( scene ); // Export the scene
-  var blob = new Blob( [str], { type : 'text/plain' } ); // Generate Blob from the string
-  saveAs( blob, 'file.stl' ); //Save the Blob to file.stl
-}
-
 
 function loadMoveHandlers() {
   renderer.domElement.addEventListener("touchstart", handleMoveZoom);
@@ -127,36 +140,57 @@ function loadMoveHandlers() {
   }
 }
 
-function loadUIBar() {
+function loadUI() {
+  // navigation bar
+  document.getElementById('clearBtn').onclick = clearObjects;
+  document.getElementById('undoBtn').onclick = undoOp;
+  document.getElementById('redoBtn').onclick = redoOp;
+  document.getElementById('doneBtn').onclick = saveObjects;
   
   // input ranges
   new WebkitInputRangeFillLower({
-    selectors: ['range1'],
+    selectors: ['aspectSlider', 'heightSlider'],
     color: 'white',
   });
 
+  let prevAspect = 1;
+
+  aspectSlider = document.getElementById('aspectSlider');
+  aspectSlider.disabled = true;
+  aspectSlider.addEventListener('input', e => {
+    let newAspect = 75 / (125 - aspectSlider.value);
+    if (currentObject) {
+      currentObject.scale.y *= newAspect / prevAspect;
+    }
+    prevAspect = newAspect;
+  });
+  
+  heightSlider = document.getElementById('heightSlider');
+  heightSlider.disabled = true;
+  heightSlider.addEventListener('input', e => {
+    if (!currentObject) return;
+    currentObject.scale.z = 75 / (125 - heightSlider.value);
+  });
   
   // shapes
   const shapes = ['★','●','■','♥','✈','A'];
   const shapePool = document.getElementById('shapePool');
-  shapes.map(text => {
+  shapes.forEach(text => {
     const div = document.createElement('div');
     div.className = 'shape';
     div.textContent = text;
     div.onclick = e => {
-      currentObject = addText(e.target.textContent);
+      addObject(genText(e.target.textContent));
     };
     shapePool.appendChild(div);
   });
 }
 
-function addText(text) {
+function genText(text) {
   if (!textToSVG) return;
   const options = {x: 0, y: 0, fontSize: 4, anchor: 'center middle', attributes: {fill: "black"}};
   const svg = textToSVG.getSVG(text, options);
-  let obj = extrudeSvg(svg);
-  scene.add(obj);
-  return obj;
+  return extrudeSvg(svg);
 }
 
 function extrudeSvg(svg) {
@@ -175,14 +209,62 @@ function extrudeSvg(svg) {
       // Finally we can take each shape and extrude it
       const geometry = new THREE.ExtrudeGeometry(shape, {
         depth: 1,
-        bevelEnabled: false
+        bevelEnabled: false,
+        // bevelThickness: 0.2,
+        // bevelSize: 0.2,
+        // bevelSegments: 8
       });
 
       // Create a mesh and add it to the group
       const mesh = new THREE.Mesh(geometry, material);
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
       svgGroup.add(mesh);
     });
   });
   svgGroup.scale.y *= -1;
   return svgGroup;
+}
+
+function addObject(obj) {
+  currentObject = null;
+  aspectSlider.disabled = false;
+  aspectSlider.value = 50;
+  aspectSlider.dispatchEvent(new Event('input'));
+  heightSlider.disabled = false;
+  heightSlider.value = 50;
+  heightSlider.dispatchEvent(new Event('input'));
+  objectList.push(obj);
+  currentObject = obj;
+  scene.add(obj);
+}
+
+function undoOp() {
+  console.log('Not implemented');
+}
+
+function redoOp() {
+  console.log('Not implemented');
+}
+
+function clearObjects() {
+  currentObject = null;
+  objectList.forEach(obj => {
+    scene.remove(obj);
+  })
+  
+  aspectSlider.value = 50;
+  aspectSlider.dispatchEvent(new Event('input'));
+  aspectSlider.disabled = true;
+  heightSlider.value = 50;
+  heightSlider.dispatchEvent(new Event('input'));
+  heightSlider.disabled = true;
+}
+
+function saveObjects() {
+  console.log('Not really implemented');
+  let exporter = new STLExporter();
+  let str = exporter.parse( scene ); // Export the scene
+  var blob = new Blob( [str], { type : 'text/plain' } ); // Generate Blob from the string
+  saveAs( blob, 'file.stl' ); //Save the Blob to file.stl
 }
